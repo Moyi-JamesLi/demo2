b49d010b17e7eb22bf01680829b0af453234faea
Moyi <puzhuo.li@moyi-tech.org>
2020-01-06 17:08:31 -0500

    practice

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # demo2
+## try by myself
---
+++ userTest.py
@@ -0,0 +1,9 @@
+# test file
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+p1 = Person("jjj", 13)
+print(p1.name)
